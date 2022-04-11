ff591947ec74fb516bb7462d1abee130d79737d6
Jitender Chauhan <66746889+JackEngineer-coder@users.noreply.github.com>
2022-04-11 19:03:49 +0530

    Create All Repeating Except Two.java
    1. You are given an array of numbers.
    2. You have to find 2 non-repeating numbers in an array.
    3. All repeating numbers are repeating even number of times.
    
    
    Sample Input
    6
    23 27 23 17 17 37
    Sample Output
    27
    37

---
+++ All Repeating Except Two.java
@@ -0,0 +1,52 @@
+import java.io.*;
+import java.util.*;
+
+public class Main {
+
+  public static void main(String[] args){
+    Scanner scn = new Scanner(System.in);
+    int n = scn.nextInt();
+    int[] arr = new int[n];
+    for(int i = 0 ; i < n; i++){
+      arr[i] = scn.nextInt();
+    }
+    solution(arr);
+  }
+
+  public static void solution(int[] arr){
+   
+   int xory=0;
+   for(int val: arr)
+   {
+       xory ^= val;
+   }
+   int rsbm = xory & -xory;
+   
+   int x =0;
+   int y =0;
+   for(int val : arr)
+   {
+       if((val & rsbm)==0)
+       {
+           x = x^val;
+       }
+       else
+       {
+           y = y^val;
+       }
+   }
+   
+   if(x>y)
+   {
+       System.out.println(y);
+       System.out.println(x);
+   }
+   else
+   {
+     System.out.println(x);
+     System.out.println(y);   
+   }
+   
+  }
+
+}
